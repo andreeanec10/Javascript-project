40aedb364ce5df202d81ac6e81add0fc7a12de61
Neculai Andreea 324CD <58053356+andreeanec10@users.noreply.github.com>
2022-02-09 22:06:48 +0200

    Adaugare validari

--- src/models/Employeer.js
+++ src/models/Employeer.js
@@ -1,18 +1,55 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 //require method returns a singleton object
 let mongoose = require('mongoose');
+let validator = require('validator');
 
 /*
   Defining DB Schema
 */
 let empSchema = new mongoose.Schema({
-    name: String,
-    address: String,
-    email: String,
-    hire_date: Date,
-    salary: Number,
-    job_title: String
+    name: {
+        type: String,
+        required: true,
+        validate: (value) => {
+            return validator.isAlpha(value);
+        }
+    },
+    address: {
+        type: String,
+        required: true,
+        validate: (value) => {
+            return validator.isAlphanumeric(value);
+        }
+    },
+    email: {
+        type: String,
+        validate: (value) => {
+            return validator.isEmail(value);
+        }
+    },
+    hire_date: {
+        type: Date,
+        required: true,
+        validate: (value) => {
+            return validator.isDate(value);
+        }
+    },
+    salary: {
+        type: Number,
+        required: true,
+        validate: (value) => {
+            return validator.isNumeric(value);
+        }
+    },
+    job_title: {
+        type: String,
+        required: true,
+        validate: (value) => {
+            return validator.isAlpha(value);
+        }
+    },
 })
 
 module.exports = mongoose.model('Employeer', empSchema);
